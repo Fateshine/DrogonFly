dc53bbf03434321fa8b37b89c8fb26758f394a97
Shih-Ying Hung <legend2409@gmail.com>
2023-01-30 17:12:03 +0800

    update

--- ICS/ICS_Interface.py
+++ ICS/ICS_Interface.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import zmq
 import paho.mqtt.client as mqtt
-from fire_sim import Sim_fire2
+from Flight_Planner_2023 import Get_sim
 from collections import defaultdict
 import random
 import cv2
@@ -20,74 +20,6 @@ def image_pocessing(fig):
     #Todo
     return
 
-def Get_sim(st,et, rseed,a,output,time_slot):  # here a is a window!!!!! 
-    lay_num=12
-    Sim_fire2(st,et,rseed,a,output,time_slot)  # write the simulation in a file.
-    Timeline=pd.read_csv(output,sep='/ ',engine='python')
-    Sim=defaultdict(dict)
-    Sim_real=defaultdict(dict)
-    win=[]
-    firs=list(Timeline['f'])
-    tmp=[i[1:-1].split(', ') for i in firs]
-    all_fire=set()
-    for i in tmp:
-        try:
-            all_fire.update([int(k) for k in i])
-        except:
-            pass
-    hum_floors=set([i//lay_num for i in all_fire]+[i//lay_num +1 for i in all_fire])
-    fire_floors=set([i//lay_num for i in all_fire])  
-    win_floors=set([i//lay_num for i in all_fire]+[i//lay_num -1 for i in all_fire])  # this layer and the lower layer. 
-    #print(f"ss {all_floors}")
-    for index,row in Timeline.iterrows():
-        #print(row)
-        tmp=row['f'][1:-1].split(', ')
-        tmps=row['f_s'][1:-1].split(', ')
-        #print(f"check  {tmp}")
-        if len(tmp)==1 and tmp[0]=='':
-            Sim[row['t']]['f']=[]
-            Sim_real[row['t']]['f']=[]
-            Sim[row['t']]['f_s']=[]
-            Sim_real[row['t']]['f_s']=[]
-        else:
-            fire=[int(tmp[i]) for i in range(len(tmp))]
-            fire_state=[int(tmps[i]) for i in range(len(tmp))]
-            Sim[row['t']]['f']=fire
-            Sim[row['t']]['f_s']=fire_state
-            Sim_real[row['t']]['f']=fire
-            Sim_real[row['t']]['f_s']=fire_state
-        tmp=row['w'][1:-1].split(', ')
-        #if len(tmp)>0:
-        winn=[int(tmp[i]) for i in range(len(tmp))]
-        win=[]
-        for i in winn:
-            if i//lay_num in win_floors: # 
-                win.append(i)
-        Sim[row['t']]['w']=win
-        Sim_real[row['t']]['w']=win
-        #except:
-            #Sim[row['t']]['w']=[]
-        tmp=row['h'][1:-1].split(', ')
-        tmps=row['h_s'][1:-1].split(', ')
-        try:
-            humm=[int(tmp[i]) for i in range(len(tmp))]
-            hum_s=[int(tmps[i]) for i in range(len(tmp))]
-            h_s=dict(zip(humm,hum_s))
-            hum=[];state=[]
-            for i in humm:
-                if i//lay_num in hum_floors:
-                    hum.append(i)
-                    state.append(h_s.get(i))
-            Sim[row['t']]['h']=hum 
-            Sim[row['t']]['h_s']=state
-            Sim_real[row['t']]['h']=hum
-            Sim_real[row['t']]['h_s']=state
-        except:
-            Sim[row['t']]['h']=[]
-            Sim[row['t']]['h_s']=[]
-            Sim_real[row['t']]['h_s']=[]
-            Sim_real[row['t']]['h']=[]     
-    return Sim,Sim_real,fire_floors, hum_floors, win_floors
 def update_fire_sim():
     floor_num=12
     va=3
@@ -199,6 +131,6 @@ while True:
         # cv2.imshow("image", source)
         # cv2.waitKey(0)
         image_pocessing(source)
-        # update_fire_sim()
+        update_fire_sim()
     finally:
         pass
